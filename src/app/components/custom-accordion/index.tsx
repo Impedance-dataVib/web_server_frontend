88c5a9e455206ce2fff91df177906d7dfedfe411
Prasun117 <johnmukherjee1997@gmail.com>
2023-05-02 13:51:30 +0530

    added accordion base component

--- src/app/components/custom-accordion/index.tsx
+++ src/app/components/custom-accordion/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+
 import { styled } from "@mui/material/styles";
 import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
 import MuiAccordion, { AccordionProps } from "@mui/material/Accordion";
