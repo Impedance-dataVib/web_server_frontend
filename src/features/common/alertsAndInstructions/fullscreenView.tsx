6b625d7ade6c946e867cedc4e0a09c4ba0a2dfad
Shailendra.Ahir <Shailendra.ahir@mphasis.com>
2023-05-16 09:35:02 +0530

    Added modal for widgets
    Signed-off-by: Shailendra.Ahir <Shailendra.ahir@mphasis.com>

---
+++ src/features/common/alertsAndInstructions/fullscreenView.tsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from "@mui/material"
+
+const FullscreenView = () => {
+
+    return (
+        <Box>
+            <Box>
+                <Typography>HI This is full screen view</Typography>
+            </Box>
+        </Box>
+    )
+}
+export default FullscreenView;
